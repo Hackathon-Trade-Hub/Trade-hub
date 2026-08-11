62a6d34d94fab482c4ab6a0bd1e0f0c7540555d5
wesleymadeira <wesleygabrielmadeira2022@gmail.com>
2026-08-11 15:02:20 -0300

    Adição de cada "espaço" no array dos produtos

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -1,4 +1,465 @@
 const produtos = [
+        //Casa e Móveis
+        {
+          id: 1,
+          titulo: "Sofá Casa Sofá Roma 3 Lugares Suede",
+          descricao: "",
+          imagem: "/images/sofa.png",
+          preco: "R$ 575,91"
+        },
+        {
+          id: 2,
+          titulo: "Moval Cômoda 8 Gavetas Georgia",
+          descricao: "",
+          imagem: "/images/comoda.png",
+          preco: "R$ 764,91"
+        },
+        {
+          id: 3,
+          titulo: "Mesa de Jantar Cone Oval Base Madeira Tampo",
+          descricao: "",
+          imagem: "/images/mesa.png",
+          preco: "R$ 2.523"
+        },
+        {
+          id: 4,
+          titulo: "Mesa Eiffel Quadrado Tampo de Madeira 90cm 4 Cadeiras Katrina",
+          descricao: "",
+          imagem: "/images/mesa_eiffel.png",
+          preco: "R$ 1.700"
+        },
+        {
+          id: 5,
+          titulo: "Aparador Buffet Sala Gold 4 Portas com Pés de Madeira Maciça Cinza/mel",
+          descricao: "",
+          imagem: "/images/aparador.png",
+          preco: "R$ 429,52"
+        },
+        {
+          id: 6,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 7,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 8,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        //Eletrodomésticos
+        {
+          id: 9,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 10,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 11,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 12,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 13,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 14,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 15,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 16,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        //Esportes
+        {
+          id: 17,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 18,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 19,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 20,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 21,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 22,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 23,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 24,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        //Ferramentas
+        {
+          id: 25,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 26,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 27,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 28,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 29,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 30,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 31,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 32,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        //Pet Shop
+        {
+          id: 33,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 34,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 35,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 36,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 37,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 38,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 39,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 40,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        //Brinquedos
+        {
+          id: 41,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 42,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 43,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 44,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 45,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 46,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 47,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 48,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        //Tecnologia
+        {
+          id: 49,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 50,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 51,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 52,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 53,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 54,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 55,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 56,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        //Acessórios
+        {
+          id: 57,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 58,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 59,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 60,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 61,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 62,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 63,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+        {
+          id: 64,
+          titulo: "",
+          descricao: "",
+          imagem: "/images/",
+          preco: "R$ "
+        },
+      ]
+
+  export {produtos as listaProdutos};
+/* 
+
         {
           id: 1,
           titulo: "PlayStation 5",
@@ -23,6 +484,4 @@ const produtos = [
           descricao: "RTX 3060, 16GB RAM. Busco MacBook M1 ou superior para trabalho.",
           imagem: ""
         }
-      ]
-
-  export {produtos as listaProdutos};
+*/
