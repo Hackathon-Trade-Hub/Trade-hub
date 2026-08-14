b5d8be62e12996ef39f289f167f57872ee8a536b
pedrocheetoz <schetzpedro297@gmail.com>
2026-08-14 15:02:23 -0300

    Feat: Cadastro Prod estilizado

--- src/Views/CadProduto.vue
+++ src/Views/CadProduto.vue
@@ -1,30 +1,35 @@
 <template>
-  <h1>
+  <h1 class="page-title">
     Preencha com as informações do produto
   </h1>
 
   <form @submit.prevent="enviarProduto">
 
-    <label>
-      Você quer
-      <input
-        type="radio"
-        name="venderTrocar"
-        value="vender"
-        v-model="tipo"
-      >
-      vender
+    <span>Você quer</span>
 
-      ou
+<label class="opcao">
+  <input
+    type="radio"
+    name="venderTrocar"
+    value="vender"
+    v-model="tipo"
+  >
+  <span class="radio-custom"></span>
+Vender
 
-      <input
-        type="radio"
-        name="venderTrocar"
-        value="trocar"
-        v-model="tipo"
-      >
-      trocar?
-    </label>
+</label>
+
+
+<label class="opcao">
+<input
+    type="radio"
+    name="venderTrocar"
+    value="trocar"
+    v-model="tipo"
+  >
+  <span class="radio-custom"></span>
+Trocar?
+</label>
 
     <br><br>
 
@@ -49,9 +54,19 @@
         width="200"
       >
     </div>
+    <div class="desc">
+      <label for="descricao">
+        Descrição do Produto:
+        <br>
+
+        <textarea name="descricao" id="descricao"></textarea>
+
+
+      </label>
+      </div>
 
     <br>
-
+<!-- SIX SEVEN -->
 
     <button type="submit">
       Cadastrar produto
@@ -81,6 +96,70 @@ function enviarProduto() {
 }
 </script>
 
-<style>
+<style scoped>
+.page-title{
+  font-size: 2.5rem;
+  text-align: center;
+  color: #1067f3;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+
+
+
+
+
+
+
+
+
+.opcao {
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
+  cursor: pointer;
+  user-select: none;
+  margin: 10px;
+}
+
+/* Esconde o radio original */
+.opcao input[type="radio"] {
+  display: none;
+}
+
+/* Círculo externo */
+.radio-custom {
+  width: 18px;
+  height: 18px;
+  border: 2px solid #94a3b8;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  transition: all 0.2s;
+}
+
+/* Quando marcado */
+.opcao input[type="radio"]:checked + .radio-custom {
+  border-color: #3b82f6;
+  background-color: #3b82f6;
+}
+
+/* Bolinha interna */
+.opcao input[type="radio"]:checked + .radio-custom::after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 8px;
+  height: 8px;
+  background: white;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+}
+
+/* Hover */
+.opcao:hover .radio-custom {
+  border-color: #3b82f6;
+}
 
 </style>
