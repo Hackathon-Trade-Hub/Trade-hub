5a66ed6a1b2b7c0a0b9f3cf6ad834d2e5680c506
wesleymadeira <wesleygabrielmadeira2022@gmail.com>
2026-08-20 11:15:55 -0300

    Começo do header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,32 @@
 <template>
     <header>
-        <div class="logo">
-            <RouterLink to="/">Trade<span>Hub</span></RouterLink>
-        </div>
+        <div class="header-top">
+            <div class="logo">
+                <RouterLink to="/">Trade<span>Hub</span></RouterLink>
+            </div>
 
-        <div class="pesquisa">
-            <img src="../../public/images/lupa.png">
-            <input type="text" placeholder="Digite...">
-        </div>
+            <div class="pesquisa">
+                <img src="../../public/images/lupa.png" alt="lupa">
+                <input type="text" placeholder="Digite...">
+            </div>
 
-        <div class="botoes">
-            <RouterLink to="/entrar" class="entrar">Entrar</RouterLink>
-            <RouterLink to="/registrar" class="login">Login</RouterLink>
-            <i class="fa-solid fa-circle-user"></i>
-            <i class="fa-solid fa-gear"></i>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+            <div class="botoes">
+                <RouterLink to="/entrar" class="entrar">ENTRAR</RouterLink>
+                <RouterLink to="/registrar" class="login">Login</RouterLink>
+                <i class="fa-solid fa-circle-user"></i>
+                <i class="fa-solid fa-gear"></i>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+            </div>
+        </div>
+        <div class="header-bottom">
+            <nav>
+                <div>
+                    Categorias <span>></span>
+                </div>
+                <RouterLink to="#">Trocar</RouterLink>
+                <RouterLink to="#">Comprar</RouterLink>
+                <RouterLink to="#">Cadstrar Produtos</RouterLink>
+            </nav>
         </div>
     </header>
 </template>
@@ -28,70 +40,62 @@ export default {
 </script>
 
 <style scoped>
-header {
-    background-color: #ffffff;
+* {
+    text-decoration: none;
+}
+
+
+.header-top {
+    display: flex;
+    margin: 1vw 0 1.5vw 1vw;
+    align-items: center;
+}
+
+.logo {
+    font-size: 2rem;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    padding: 15px 60px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    font-weight: 600;
 }
+
 .logo a {
-    font-size: 3vw;
-    font-weight: 700;
-    color: #0b1a30;
-    text-decoration: none;
+    color: black;
 }
+
 .logo a span {
-    color: #0066ff;
+    color: blue;
 }
+
 .pesquisa {
+    position: relative;
     display: flex;
     align-items: center;
+    margin-left: 5vw;
 }
-.pesquisa img {
-    width: 3vw;
-    height: 3vw;
-}
-.pesquisa input{
-    border-radius: 30px;
+
+.pesquisa input {
+    width: 30vw;
+    height: 2.5vw;
+    padding-left: 2.5vw;
     border: 1px solid black;
-    padding: 4px 16px;
-    height: 2vw;
-    width: 25vw;
+    border-radius: 16px;
+    font-size: 1rem;
 }
+
+.pesquisa img {
+    position: absolute;
+    left: 0.8vw;
+    width: 1.3vw;
+    height: 1.3vw;
+}
+
 .botoes {
-    display: flex;
-    align-items: center;
-    gap: 15px;
+    margin-left: 10vw;
 }
-.entrar {
-    color: #0066ff;
-    font-weight: 600;
-    padding: 8px 16px;
-    text-decoration: none;
-    border: 1px solid #0066ff;
-    padding: 10px 20px;
-    border-radius: 6px;
-    transition: 0.3s;
-}
-.entrar:hover {
-    background: #f5f5f5;
-}
-.login {
-    background-color: #0066ff;
-    color: #ffffff;
-    font-weight: 600;
-    padding: 10px 20px;
-    border-radius: 6px;
-    text-decoration: none;
-    transition: 0.3s;
-}
-.login:hover {
-    background-color: #004ecc;
-}
-i{
-    font-size: 2.2vw;
-    cursor: pointer;
+
+.entrar{
+    border: 1px solid black;
+    color: black;
+    border-radius: 8px;
+    padding: 0.75vw 1.75vw;
 }
 </style>
