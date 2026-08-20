c257953d8802536e7b0b66414f7601c08c3a712a
RianOliveira2112 <riandavioliveira@gmail.com>
2026-08-20 11:14:50 -0300

    progresso

--- src/components/ProdutoCard.vue
+++ src/components/ProdutoCard.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="card" style="width: 18rem;">
-    <img :src="imagem" class="card-img-top" :alt="titulo">
+  <div class="card">
+    <div class="caixa-imagem">
+      <img :src="imagem" class="card-img-top" :alt="titulo">
+    </div>
     <div class="card-body">
       <h5 class="card-title">{{ titulo }}</h5>
-      <p class="card-text">{{ descricao }}</p>
       <a href="#" class="btn-primary">Acessar</a>
     </div>
   </div>
@@ -21,9 +22,21 @@ export default {
 </script>
 
 <style scoped>
+.card{
+  width: 18rem;
+  border: 2px solid #0066ff !important;
+  margin: 10px;
+}
+.caixa-imagem{
+  width: 16vw;
+  height: 16vw;
 
+}
 .card-img-top{
   border-radius: 5px;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 .card-title{
   color: #0066ff;
@@ -31,12 +44,6 @@ export default {
   font-size: 4vh;
 
   }
-.card-text{
-  color: #555555;
-  font-weight: 500;
-  font-size: 17px;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
-}
 .btn-primary{
   background-color:#0066ff;
   text-decoration: none;
