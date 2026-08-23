9814080820bc539acb2ff66604b248fc4682093b
TradeHubIFC <tradehubifc@gmail.com>
2026-08-23 18:12:02 -0300

    categoriaProduto criado linkado no header, adicionado no Router e paginaProduto Corrigida

---
+++ src/Views/catProd.vue
@@ -0,0 +1,28 @@
+<template>
+ <h1>{{ route.params.categoria }}</h1>
+
+<ProdutoCard
+  v-for="produto in produtosDaCategoria"
+  :key="produto.id"
+  :id="produto.id"
+  :titulo="produto.titulo"
+  :descricao="produto.descricao"
+  :imagem="produto.imagem">
+</ProdutoCard>
+
+</template>
+<script setup>
+import { listaProdutos } from '@/data/produtos';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import ProdutoCard from '@/components/ProdutoCard.vue';
+
+const route = useRoute();
+
+const produtosDaCategoria = computed(() => {
+  const idDaUrl = Number(route.params.categoria);
+  return listaProdutos.filter(item => item.categoria === idDaUrl);
+});
+</script>
+<style scoped>
+</style>
