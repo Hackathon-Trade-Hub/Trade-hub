021963a177d89248b5100666c4ae8a2e9a4e930f
enzobernardes <bernardesenzo9@gmail.com>
2026-08-25 21:52:39 -0300

    Adicionar condição para o header não aparecer na página do produto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="layout-app">
-    <HeaderComponent />
+    <HeaderComponent  v-if="$route.name !== 'paginaProduto'" />
 
     <main class="conteudo-principal">
       <router-view />
