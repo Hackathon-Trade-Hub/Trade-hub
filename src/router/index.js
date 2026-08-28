abac0e7eeb16511d9b55fced3dff5a2a71b6c3e3
TradeHubIFC <tradehubifc@gmail.com>
2026-08-27 23:34:01 -0300

    header corrigido e index.js corrigido tbm

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,6 @@ import PerfilVendedor from '@/Views/PerfilVendedor.vue'
 import paginaUsuario from '@/Views/paginaUsuario.vue'
 import paginaTroca from '@/Views/paginaTroca.vue'
 import PropostaTroca from '@/Views/PropostaTroca.vue'
-import Comprar from '@/Views/Comprar.vue'
 import { usuarioAtual } from '@/data/auth.js'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -65,14 +64,9 @@ const router = createRouter({
       meta: { requerLogin: true }
     },
     {
-      path: '/perfilVendedor',
-      name: 'perfilVendedor',
-      component: PerfilVendedor
-        },
-    {
-      path: '/comprar',
-      name: 'comprar',
-      component: Comprar
+  path: '/perfilVendedor',
+  name: 'perfilVendedor',
+  component: PerfilVendedor
     },
     {
       path: '/paginaTroca/:status',
